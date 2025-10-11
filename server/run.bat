6a278fcd0a58431dbe3cf6098bf07ff4121774b2
YooHyeonJun <hjyoo621@naver.com>
2025-10-11 21:14:48 +0900

    d

--- server/run.bat
+++ server/run.bat
@@ -2,13 +2,27 @@
 setlocal
 
 cd /d %~dp0
+
+REM Default env (can be overridden by caller)
+if "%PORT%"=="" set PORT=8000
+if "%HF_MODEL_ID%"=="" set HF_MODEL_ID=beomi/KcELECTRA-base-v2022
+
+REM Create/activate venv
 if not exist .venv (
 	python -m venv .venv
 )
 call .venv\Scripts\activate.bat
+
 python -m pip install --upgrade pip
 pip install -r requirements.txt
-set PORT=8000
+
+REM Ensure model dir exists one level up from server/
+set MODEL_DIR=%~dp0..\model
+if not exist "%MODEL_DIR%" (
+	mkdir "%MODEL_DIR%"
+)
+
+REM Run app from server directory
 python app.py
 
 endlocal
